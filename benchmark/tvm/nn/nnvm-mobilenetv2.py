affb03e7e77861ae4db9ad0dcb7e74b173ae7a5d
KnowingNothing <zhengsizeMax@outlook.com>
2019-09-21 10:01:46 +0800

    mobilenet-v2 still under progress

--- benchmark/tvm/nn/nnvm-mobilenetv2.py
+++ benchmark/tvm/nn/nnvm-mobilenetv2.py
@@ -9,17 +9,21 @@ from https://github.com/tonylins/pytorch-mobilenet-v2
 
 
 def generate_parameters(obj):
-    for key, value in obj.__dict__.items():
-        if isinstance(value, sym.Variable):
-            yield value
-        elif isinstance(value, Layer):
-            generate_parameters(value)
-        elif isinstance(value, (list, tuple)):
-            for ele in value:
-                generate_parameters(ele)
-        elif isinstance(value, dict):
-            for k, v in value.items():
-                generate_parameters(v)
+    if hasattr(obj, "__dict__"):
+        for key, value in obj.__dict__.items():
+            if isinstance(value, sym.Symbol):
+                yield value
+            elif isinstance(value, Layer):
+                for ret in generate_parameters(value):
+                    yield ret
+            elif isinstance(value, (list, tuple)):
+                for ele in value:
+                    for ret in generate_parameters(ele):
+                        yield ret
+            elif isinstance(value, dict):
+                for k, v in value.items():
+                    for ret in generate_parameters(v):
+                        yield ret
 
 
 class Layer(object):
@@ -30,7 +34,7 @@ class Layer(object):
         return self.forward(*args, **kwargs)
 
     def parameters(self):
-        generate_parameters(self)
+        return generate_parameters(self)
 
 
 def check_array(data, whole_type, inner_type, length):
@@ -44,7 +48,11 @@ def check_array(data, whole_type, inner_type, length):
 def compose(f, *args):
     if len(args) == 0:
         return f 
-    return compose(lambda *a, **k: args[0](f(*a, **k)), *args[1:])
+    return compose(lambda *a: args[0](f(*a)), *args[1:])
+
+
+def identity(*args):
+    return args
 
 
 class Sequential(Layer):
@@ -52,16 +60,19 @@ class Sequential(Layer):
         super(Sequential, self).__init__()
         self._call_list = args
 
-    def forward(self, *inputs):
-        ret = inputs 
-        for f in self._call_list:
-            ret = f(ret)
-        return ret
-
+    def forward(self, inputs):
+        # return compose(identity, *self._call_list)(*inputs)
+        if len(self._call_list) == 0:
+            return inputs 
+        else:
+            ret = self._call_list[0](inputs)
+            for f in self._call_list[1:]:
+                ret = f(ret)
+            return ret
 
 
 class Conv2d(Layer):
-    def __init__(self, in_channel, out_channel, kernel_size, strides=1, padding=1, dilation=1, group=1, use_bias=False, dtype="float32"):
+    def __init__(self, name, in_channel, out_channel, kernel_size, strides=1, padding=1, dilation=1, group=1, use_bias=False):
         if isinstance(strides, int):
             strides = (strides, strides) 
         if isinstance(kernel_size, int):
@@ -77,6 +88,7 @@ class Conv2d(Layer):
         check_array(padding, tuple, int, 2)
         check_array(dilation, tuple, int, 2)
 
+        self.name = name
         self._in_channel = in_channel
         self._out_channel = out_channel
         self._kernel_size = kernel_size
@@ -84,35 +96,29 @@ class Conv2d(Layer):
         self._strides = strides
         self._dilation = dilation 
         self._group = group 
-        self._dtype = dtype 
         self._use_bias = use_bias 
 
-        self.weight = sym.Variable("weight", shape=[out_channel, in_channel, *kernel_size])
+        self.weight = sym.Variable("%s_weight" % name, shape=[out_channel, in_channel, *kernel_size])
         if use_bias:
-            self.bias = sym.Variable("bias", shape=[out_channel])
+            self.bias = sym.Variable("%s_bias" % name, shape=[out_channel])
 
     def forward(self, inputs):
         if self._use_bias:
-            return sym.conv2d(inputs, weight=self.weight, bias=self.bias, channels=self._out_channel, 
+            return sym.conv2d(data=inputs, weight=self.weight, bias=self.bias, channels=self._out_channel, 
                     kernel_size=self._kernel_size, padding=self._padding, strides=self._strides, 
-                    dilation=self._dilation, group=self._group, dtype=self._dtype, use_bias=self._use_bias)
+                    dilation=self._dilation, groups=self._group, use_bias=self._use_bias)
         else:
-            return sym.conv2d(inputs, weight=self.weight, channels=self._out_channel, 
+            return sym.conv2d(data=inputs, weight=self.weight, channels=self._out_channel, 
                     kernel_size=self._kernel_size, padding=self._padding, strides=self._strides, 
-                    dilation=self._dilation, group=self._group, dtype=self._dtype, use_bias=self._use_bias)
+                    dilation=self._dilation, groups=self._group, use_bias=self._use_bias)
 
 
 class BatchNorm(Layer):
-    def __init__(self, shape):
+    def __init__(self):
         super(BatchNorm, self).__init__()
-        self.beta = sym.Variable("beta", shape=shape)
-        self.gamma = sym.Variable("gamma", shape=shape)
-        self.moving_var = sym.Variable("moving_var", shape=shape)
-        self.moving_mean = sym.Variable("moving_mean", shape=shape)
 
     def forward(self, inputs):
-        return sym.batch_norm(inputs, gamma=self.gamma, beta=self.beta, 
-                    moving_mean=self.moving_mean, moving_var=self.moving_var)
+        return sym.batch_norm(inputs)
 
 
 class ReLU(Layer):
@@ -129,43 +135,44 @@ class Dropout(Layer):
         self._rate = rate
 
     def forward(self, inputs):
-        return sym.dropout(inputs, rate=self._rate)
+        return sym.dropout(data=inputs, rate=self._rate)
 
 
 class Dense(Layer):
-    def __init__(self, in_dim, units, use_bias=False):
+    def __init__(self, name, in_dim, units, use_bias=False):
         super(Dense, self).__init__()
+        self.name = name
         self._units = units
-        self.weight = sym.Variable("weight", shape=[in_dim, units])
+        self.weight = sym.Variable("%s_weight" % name, shape=[units, in_dim])
         if use_bias:
-            self.bias = sym.Variable("bias", shape=[units])
+            self.bias = sym.Variable("%s_bias" % name, shape=[units])
         self._use_bias = use_bias
 
     def forward(self, inputs):
         if self._use_bias:
-            return sym.dense(inputs, weight=self.weight, bias=self.bisa, units=self._units)
+            return sym.dense(data=inputs, weight=self.weight, bias=self.bias, units=self._units)
         else:
-            return sym.dense(inputs, weight=self.weight, units=self._units)
+            return sym.dense(data=inputs, weight=self.weight, units=self._units)
 
 
-def conv_bn(in_channel, out_channel, kernel_size, strides=1, padding=1, dilation=1, group=1, use_bias=False, dtype="float32"):
+def conv_bn(name, in_channel, out_channel, kernel_size, strides=1, padding=1, dilation=1, group=1, use_bias=False):
     return Sequential(
-        Conv2d(in_channel, out_channel, kernel_size, strides, padding, dilation, group, use_bias, dtype),
-        BatchNorm([]),
+        Conv2d(name, in_channel, out_channel, kernel_size, strides, padding, dilation, group, use_bias),
+        BatchNorm(),
         ReLU()
     )
 
 
-def conv_3x3_bn(out_channel, strides):
-    return conv_bn(out_channel, 3, strides)
+def conv_3x3_bn(name, in_channel, out_channel, strides):
+    return conv_bn(name, in_channel, out_channel, 3, strides)
 
 
-def conv_1x1_bn(out_channel):
-    return conv_bn(out_channel, 1, padding=0)
+def conv_1x1_bn(name, in_channel, out_channel):
+    return conv_bn(name, in_channel, out_channel, 1, padding=0)
 
 
 class InvertedResidual(Layer):
-    def __init__(self, in_channel, out_channel, strides, expand_ratio):
+    def __init__(self, name, in_channel, out_channel, strides, expand_ratio):
         super(InvertedResidual, self).__init__()
         self.strides = strides 
         assert strides in [1, 2]
@@ -176,25 +183,25 @@ class InvertedResidual(Layer):
         if expand_ratio == 1:
             self.conv = Sequential(
                 # depthwise
-                Conv2d(hidden_dim, 3, strides=strides, padding=1, group=hidden_dim),
+                Conv2d("%s_depthwise_1" % name, in_channel, hidden_dim, 3, strides=strides, padding=1, group=hidden_dim),
                 BatchNorm(),
                 ReLU(),
                 # pointwise
-                Conv2d(out_channel, 1, strides=1, padding=0),
+                Conv2d("%s_pointwise_1" % name, hidden_dim, out_channel, 1, strides=1, padding=0),
                 BatchNorm()
             )
         else:
             self.conv = Sequential(
                 # pointwise
-                Conv2d(hidden_dim, 1, strides=1, padding=0),
+                Conv2d("%s_pointwise_1" % name, in_channel, hidden_dim, 1, strides=1, padding=0),
                 BatchNorm(),
                 ReLU(),
                 # depthwise
-                Conv2d(hidden_dim, 3, strides=strides, padding=1, group=hidden_dim),
+                Conv2d("%s_depthwise_1" % name, hidden_dim, hidden_dim, 3, strides=strides, padding=1, group=hidden_dim),
                 BatchNorm(),
                 ReLU(),
                 # pointwise
-                Conv2d(out_channel, 1, strides=1, padding=0),
+                Conv2d("%s_pointwise_2" % name, hidden_dim, out_channel, 1, strides=1, padding=0),
                 BatchNorm()
             )
 
@@ -206,7 +213,7 @@ class InvertedResidual(Layer):
 
 
 class MobileNetV2(Layer):
-    def __init__(self, n_class=1000, input_size=224, width_mult=1):
+    def __init__(self, name, n_class=1000, input_size=224, width_mult=1):
         super(MobileNetV2, self).__init__()
         block = InvertedResidual
         input_channel = 32
@@ -214,34 +221,36 @@ class MobileNetV2(Layer):
         interverted_residual_setting = [
             # expand_ratio, c, n, stride
             [1, 16, 1, 1],
-            [6, 24, 2, 2],
-            [6, 32, 3, 2],
-            [6, 64, 4, 2],
-            [6, 96, 3, 1],
-            [6, 160, 3, 2],
-            [6, 320, 1, 1]
+            # [6, 24, 2, 2],
+            # [6, 32, 3, 2],
+            # [6, 64, 4, 2],
+            # [6, 96, 3, 1],
+            # [6, 160, 3, 2],
+            # [6, 320, 1, 1]
         ]
 
         assert input_size % 32 == 0
         input_channel = int(input_channel * width_mult)
         self.last_channel = int(last_channel * width_mult) if width_mult > 1.0 else last_channel
-        self.features = [conv_3x3_bn(input_channel, 2)]
+        self.features = [conv_3x3_bn("%s_first_conv3x3_bn" % name, 3, input_channel, 2)]
 
         for t, c, n, s in interverted_residual_setting:
             output_channel = int(c * width_mult)
             for i in range(n):
                 if i == 0:
-                    self.features.append(block(input_channel, output_channel, s, expand_ratio=t))
+                    self.features.append(block("%s_residual_%d" % (name, i+1), 
+                                    input_channel, output_channel, s, expand_ratio=t))
                 else:
-                    self.features.append(block(input_channel, output_channel, 1, expand_ratio=t))
+                    self.features.append(block("%s_residual_%d" % (name, i+1), 
+                                    input_channel, output_channel, 1, expand_ratio=t))
                 input_channel = output_channel
                 
-        self.features.append(conv_1x1_bn(self.last_channel))
+        self.features.append(conv_1x1_bn("%s_last_conv1x1_bn" % name, input_channel, self.last_channel))
         self.features = Sequential(*self.features)
 
         self.classifier = Sequential(
             Dropout(0.2),
-            Dense(n_class)
+            Dense("%s_dense" % name, self.last_channel, n_class)
         )
 
     def forward(self, inputs):
@@ -250,3 +259,15 @@ class MobileNetV2(Layer):
         y = self.classifier(y)
         return y 
 
+
+if __name__ == "__main__":
+    net = MobileNetV2("mobilenet-v2")
+    
+    inputs = sym.Variable("inputs", shape=[111, 3, 224, 224])
+    output = net(inputs)
+
+    compute_graph = nnvm.graph.create(output)
+    print(compute_graph.ir())
+
+    deploy_graph, lib, params = nnvm.compiler.build(compute_graph, target="cuda")
+    print(deploy_graph.ir())
